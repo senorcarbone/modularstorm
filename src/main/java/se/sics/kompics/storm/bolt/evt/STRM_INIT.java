4596de868da0075dba17821e2409d75cd92a7247
senorcarbone <seniorcarbone@gmail.com>
2013-05-02 19:30:00 +0200

    changed STRM_INIT to Event type

--- src/main/java/se/sics/kompics/storm/bolt/evt/STRM_INIT.java
+++ src/main/java/se/sics/kompics/storm/bolt/evt/STRM_INIT.java
@@ -1,12 +1,13 @@
 package se.sics.kompics.storm.bolt.evt;
 
 import backtype.storm.task.TopologyContext;
+import se.sics.kompics.Event;
 import se.sics.kompics.Init;
 
 /**
  * @author carbone
  */
-public class STRM_INIT extends Init {
+public class STRM_INIT extends Event {
 
     private final TopologyContext topologyContext;
 
